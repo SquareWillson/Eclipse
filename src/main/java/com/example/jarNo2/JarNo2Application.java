d6135fa59c0e24429a4a2c4f4149a126595020dc
zhalin <zhalin@163.com>
2018-07-17 11:10:33 +0800

    commit

---
+++ src/main/java/com/example/jarNo2/JarNo2Application.java
@@ -0,0 +1,12 @@
+package com.example.jarNo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarNo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarNo2Application.class, args);
+	}
+}
